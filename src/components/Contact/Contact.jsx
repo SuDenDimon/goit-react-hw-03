9fd4b92059a3b0182c2738a06e6a81062162d0ed
SuDenDimon <sgp7766@gmail.com>
2025-05-04 17:48:46 +0300

    contact-style

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,16 +1,20 @@
-import { FaUser, FaPhone } from 'react-icons/fa';
+import { FaUser, FaPhoneAlt } from 'react-icons/fa';
 import style from './Contact.module.css';
 
 const Contact = ({ name, number }) => {
   return (
     <div className={style.contactItem}>
-      <div className="contact-info">
-        <FaUser /> {name}
+      <div className={style.infoGroup}>
+        <div className={style.contactInfo}>
+          <FaUser /> {name}
+        </div>
+        <div className={style.contactInfo}>
+          <FaPhoneAlt /> {number}
+        </div>
       </div>
-      <div className="contact-info">
-        <FaPhone /> {number}
-      </div>
-      <button type="button">Delete</button>
+      <button type="button" className={style.deleteBtn}>
+        Delete
+      </button>
     </div>
   );
 };
